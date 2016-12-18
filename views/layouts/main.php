17960bd8740d1b06ab0bb6cd74ec904b5c199529
root <root@kinoextra.com>
2016-12-18 09:41:49 -0500

    add yandex metriks

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -193,6 +193,7 @@ use app\widgets\LastEstimations\LastEstimations;
                                         " часа, посетителей за 24 часа и за сегодня' " +
                                         "border='0' width='88' height='31'><\/a>")
                                 //--></script><!--/LiveInternet-->
+                                <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter17510455 = new Ya.Metrika({ id:17510455, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/17510455" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
                         </div>
                     </div>
                 </div>
